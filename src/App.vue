f452cae86c971c6d1aeb49a123ebbd48470c58d4
OpenLearn <tmoropodi@gmail.com>
2024-08-29 19:31:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<!-- https://www.youtube.com/watch?v=VeNfHj6MhgA&t=4990s -->
+<!-- https://github.com/bradtraversy/vue-crash-2024/tree/main -->
+
+<template>
+  <Navbar />
+  <RouterView />
+</template>
